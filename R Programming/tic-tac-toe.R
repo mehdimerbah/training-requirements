e147dd9a35d1e22c67a47d4887553bf24536b455
mehdimerbah <mehdimerbah@outlook.com>
2021-04-17 03:25:02 +0100

    Updated R tic-tac-toe

--- R Programming/tic-tac-toe.R
+++ R Programming/tic-tac-toe.R
@@ -9,13 +9,39 @@ if (interactive()) {
 cat("X or O? ")
 symbol <- readLines(con = con, n = 1)
 
-tic_mat <- matrix(x, nrow = 3, ncol = 3)
+tic_mat <- matrix(, nrow = 3, ncol = 3)
 
-check_if_win() <- function(){
+check_if_win() <- function(mat){
+	x_sum <- utf8ToInt("X")*3
+	o_sum <- utf8ToInt("O")*3
+	pos_diag_sum = 0
+	neg_diag_sum = 0
+	for(i in 1:3){
+	check_sum_row = 0
+	check_sum_col = 0
+		for(j in 1:3){
+		check_sum_row <- check_sum_row + utf8ToInt(mat[i,j])
+		check_sum_col <- check_sum_col + utf8ToInt(mat[j,i])
+			if(i==j){
+				pos_diag_sum <- pos_diag_sum + utf8ToInt(mat[i,j])
+			}
+		}
+		neg_idx <- 4-i
+		neg_diag_sum <- neg_diag_sum + utf8ToInt(mat[i,neg_idx])
+		if(check_sum_row == x_sum | check_sum_col == x_sum){
+			return("X")
+		}
+		else if(check_sum_row == o_sum | check_sum_col == o_sum){
+			return("O")
+		}
 
-	for(i in 1:10){
-		if
-	
+	}
+	if(pos_diag_sum == x_sum | neg_diag_sum == x_sum){
+		return("X")
+	}
+	else if(pos_diag_sum == o_sum | neg_diag_sum == o_sum){
+		return("O")
 	}
 
+return("Tie")
 }
